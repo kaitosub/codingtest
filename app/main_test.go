5eaf3529f6c0250b94dc39654cebca9ca7ff2748
sugisawakaito <kaito24262426@gmail.com>
2022-03-02 06:43:40 +0900

    refactor: httpのaddrを8888に変更

--- app/main_test.go
+++ app/main_test.go
@@ -53,7 +53,7 @@ func TestCreate(t *testing.T) {
 				resp, err := http.DefaultClient.Do(req)
 				if err != nil {
 					t.Error("row: %i", row)
-					t.Error(req, resp, err)
+					t.Error(err)
 					return
 				}
 
