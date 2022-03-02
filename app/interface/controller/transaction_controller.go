ac9dd7098ae9fabe81010860e07aeaf5f9c7e844
sugisawakaito <kaito24262426@gmail.com>
2022-03-02 19:57:15 +0900

    refactor: uIDを確認

--- app/interface/controller/transaction_controller.go
+++ app/interface/controller/transaction_controller.go
@@ -6,7 +6,6 @@ import (
 	"github.com/kaitosub/codingtest/app/infrastructure/mysql"
 	"github.com/kaitosub/codingtest/app/interface/database"
 	"github.com/kaitosub/codingtest/app/usecase"
-	"github.com/kaitosub/codingtest/app/util/ctx"
 	"log"
 	"net/http"
 	"strconv"
@@ -33,20 +32,14 @@ func (tr *TransactionController) GetTransactions(w http.ResponseWriter, r *http.
 		log.Fatalln("33:", err)
 		return
 	}
-	var transactionRequest model.Transaction
+	var transactionRequest model.TransactionRequest
 	err = json.Unmarshal(body, &transactionRequest)
 	if err != nil {
 		log.Fatalln("39:", err)
 		return
 	}
 
-	ctxUser, err := ctx.GetCtxUser(r.Context())
-	if err != nil {
-		log.Fatalln("45:", err)
-		return
-	}
-
-	transaction := model.Transaction{UserId: ctxUser.ID, Amount: transactionRequest.Amount, Description: transactionRequest.Description}
+	transaction := model.TransactionEntity{UserID: transactionRequest.UserId, Amount: transactionRequest.Amount, Description: transactionRequest.Description}
 
 	//var amount int
 	//if err := mysql.DB.QueryRow(
@@ -81,7 +74,7 @@ func (tr *TransactionController) GetTransactions(w http.ResponseWriter, r *http.
 	w.WriteHeader(201)
 }
 
-func InsertTransaction(transaction model.Transaction) (id int, err error) {
-	_, err = mysql.DB.Exec("INSERT INTO transaction (user_id, amount, description) VALUES (?, ?, ?)", transaction.UserId, transaction.Amount, transaction.Description)
+func InsertTransaction(transaction model.TransactionEntity) (id int, err error) {
+	_, err = mysql.DB.Exec("INSERT INTO transaction (user_id, amount, description) VALUES (?, ?, ?)", transaction.UserID, transaction.Amount, transaction.Description)
 	return id, err
 }
