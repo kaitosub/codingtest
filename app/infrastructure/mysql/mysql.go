07f47e3c73b58ad5acb7ea4cb1d428229864aca7
sugisawakaito <kaito24262426@gmail.com>
2022-03-09 22:57:58 +0900

    test修正

--- app/infrastructure/mysql/mysql.go
+++ app/infrastructure/mysql/mysql.go
@@ -10,13 +10,6 @@ var DB *sql.DB
 
 func Connect() {
 	DBMS := "mysql"
-	//dbInfo := fmt.Sprintf(
-	//	"%s:%s@%s/%s?parseTime=true",
-	//	"go_test",
-	//	"root",
-	//	"tcp@127.0.0.1:3306",
-	//	"go_database",
-	//)
 	db, err := sql.Open(DBMS, "root@tcp(127.0.0.1)/codetest")
 	if err != nil {
 		fmt.Println("error")
